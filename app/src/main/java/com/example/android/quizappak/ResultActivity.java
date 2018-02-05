d1b6686a2bc3915be4c72f2d58584894c742ac40
AKebire <34797884+AKebire@users.noreply.github.com>
2018-02-05 23:26:38 +0000

    Add files via upload

--- app/src/main/java/com/example/android/quizappak/ResultActivity.java
+++ app/src/main/java/com/example/android/quizappak/ResultActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -26,17 +27,33 @@ public class ResultActivity extends AppCompatActivity {
         if(score >= 3)
         {
             quizResult += "You passed the quiz, you answered " + score + " correct answers.";
+            Toast.makeText(this, "Congratulation you passed the test.", Toast.LENGTH_SHORT).show();
         }else
         {
             quizResult += "You failed the quiz, you answered " + score + " correct answers.";
+            Toast.makeText(this, "You failed the quiz, please try it again.", Toast.LENGTH_SHORT).show();
         }
 
         return quizResult;
     }
 
+    /*
+     *  The displayMessage is used to show the result of the quiz.
+     */
     private void displayMessage(String scoreMessage) {
         TextView scoreTextView = findViewById (R.id.score_text_view);
         scoreTextView.setText (scoreMessage);
       }
+
+
+    /*
+     * This method is used to prevent the user from going back to the quiz using return button.
+     */
+    @Override
+    public void onBackPressed()
+    {
+        Toast.makeText(this, "You can't go back to the quiz you took.", Toast.LENGTH_SHORT).show();
+
+    }
 
 }
