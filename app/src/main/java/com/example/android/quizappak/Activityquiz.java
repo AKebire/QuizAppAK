bdcbfe60c9a2bd1b73ff74559743cfbf841cde3c
AKebire <34797884+AKebire@users.noreply.github.com>
2018-02-06 22:43:39 +0000

    Add files via upload

--- app/src/main/java/com/example/android/quizappak/Activityquiz.java
+++ app/src/main/java/com/example/android/quizappak/Activityquiz.java
@@ -30,28 +30,29 @@ public class Activityquiz extends AppCompatActivity {
         CheckBox q3FirstAnswer =  findViewById(R.id.q3_answer_1);
         CheckBox q3SecondAnswer =  findViewById(R.id.q3_answer_2);
         EditText q4Answer = findViewById(R.id.q4_answer);
+        RadioButton q5Answer = findViewById(R.id.q5_answer);
+        RadioButton q6Answer = findViewById(R.id.q6_answer);
+        RadioButton q7Answer = findViewById(R.id.q7_answer);
+        RadioButton q8Answer = findViewById(R.id.q8_answer);
+
 
         q4QuizAnswer = q4Answer.getText().toString();
 
-        if(q1Answer.isChecked())
-        {
-            result += 1;
-        }
+        if(q1Answer.isChecked()) { result += 1; }
 
-        if (q2Answer.isChecked())
-        {
-            result += 1;
-        }
+        if (q2Answer.isChecked()) {  result += 1;}
 
-        if (q3FirstAnswer.isChecked() && q3SecondAnswer.isChecked())
-        {
-            result += 1;
-        }
+        if (q3FirstAnswer.isChecked() && q3SecondAnswer.isChecked())  { result += 1; }
 
-        if (q4QuizAnswer.equals("Asia"))
-        {
-            result += 1;
-        }
+        if (q4QuizAnswer.equals("Asia")) { result += 1; }
+
+        if(q5Answer.isChecked())  {  result += 1; }
+
+        if (q6Answer.isChecked()) {  result += 1; }
+
+        if(q7Answer.isChecked())  {  result += 1; }
+
+        if (q8Answer.isChecked()) {  result += 1; }
 
         return result;
     }
--- app/src/main/java/com/example/android/quizappak/ResultActivity.java
+++ app/src/main/java/com/example/android/quizappak/ResultActivity.java
@@ -1,8 +1,10 @@
 package com.example.android.quizappak;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,18 +22,31 @@ public class ResultActivity extends AppCompatActivity {
         displayMessage(scoreMessage);
     }
 
+    /*
+     * Display the result of the quiz.
+     */
+
 
     private String checkScore() {
         String quizResult = "";
 
-        if(score >= 3)
+        if(score >= 5)
         {
             quizResult += "You passed the quiz, you answered " + score + " correct answers.";
-            Toast.makeText(this, "Congratulation you passed the test.", Toast.LENGTH_SHORT).show();
+            Toast passTest = Toast.makeText(this, "Congratulation you passed the test.", Toast.LENGTH_SHORT);
+            passTest.setGravity(Gravity.CENTER,0,0);
+            TextView v = (TextView) passTest.getView().findViewById(android.R.id.message);
+            v.setTextColor(Color.YELLOW);
+            passTest.show();
         }else
         {
             quizResult += "You failed the quiz, you answered " + score + " correct answers.";
-            Toast.makeText(this, "You failed the quiz, please try it again.", Toast.LENGTH_SHORT).show();
+            Toast failTest = Toast.makeText(this, "You failed the quiz, please try it again.", Toast.LENGTH_LONG);
+            failTest.setGravity(Gravity.CENTER,0,0);
+            failTest.setGravity(Gravity.CENTER,0,0);
+            TextView v = (TextView) failTest.getView().findViewById(android.R.id.message);
+            v.setTextColor(Color.YELLOW);
+            failTest.show();
         }
 
         return quizResult;
@@ -47,13 +62,32 @@ public class ResultActivity extends AppCompatActivity {
 
 
     /*
-     * This method is used to prevent the user from going back to the quiz using return button.
+     * This method is used to prevent the user from going back to the quiz using return button once finished by displaying a message.
      */
     @Override
     public void onBackPressed()
     {
-        Toast.makeText(this, "You can't go back to the quiz you took.", Toast.LENGTH_SHORT).show();
-
+        Toast returnToQuiz = Toast.makeText(this, "You can't go back to the quiz you took.", Toast.LENGTH_LONG);
+        returnToQuiz.setGravity(Gravity.CENTER,0,0);
+        TextView v = (TextView) returnToQuiz.getView().findViewById(android.R.id.message);
+        v.setTextColor(Color.YELLOW);
+        returnToQuiz.show();
     }
+
+
+    public void playAgain(View view) {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void exitApp(View view) {
+
+        moveTaskToBack(true);
+        android.os.Process.killProcess(android.os.Process.myPid());
+        System.exit(1);
+    }
+
+
 
 }
